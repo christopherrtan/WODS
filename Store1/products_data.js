43a40b3ba9c0033189ad96e8136c7c9fc6af02df
christopherrtan <143047553+christopherrtan@users.noreply.github.com>
2023-10-29 23:20:24 -1000

    store1
    store1

---
+++ Store1/products_data.js
@@ -0,0 +1,7 @@
+const products = [
+    { brand: 'HTC', price: 40.00 },
+    { brand: 'Apple', price: 75.00 },
+    { brand: 'Nokia', price: 35.00 },
+    { brand: 'Samsung', price: 45.00 },
+    { brand: 'Blackberry', price: 10.00 }
+];
